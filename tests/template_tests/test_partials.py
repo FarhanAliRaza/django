c5dccb4492b54315b8b1fb20dbb31264ef5cb593
farhan <farhanalirazaazeemi@gmail.com>
2025-05-22 23:24:21 +0500

    updated tests

--- tests/template_tests/test_partials.py
+++ tests/template_tests/test_partials.py
@@ -3,7 +3,7 @@ Testing template partials.
 
 """
 
-from django.template import engines
+from django.template import Context, engines
 from django.test import TestCase
 
 
@@ -17,7 +17,18 @@ class PartialTagsTestCase(TestCase):
         # Compile and render the template
         engine = engines["django"]
         t = engine.from_string(template)
-        rendered = t.render()
+        rendered = t.render(Context({}))
 
         # Check the rendered content
         self.assertEqual("HERE IS THE TEST CONTENT", rendered.strip())
+
+    def test_just_partial_from_loader(self):
+        engine = engines["django"]
+
+        template = engine.get_template("example.html#test-partial")
+        rendered = template.render(Context({}))
+        self.assertEqual("TEST-PARTIAL-CONTENT", rendered.strip())
+
+        template = engine.get_template("example.html#inline-partial")
+        rendered = template.render(Context({}))
+        self.assertEqual("INLINE-CONTENT", rendered.strip())
